97badf9af9656d153008f34d60e6a3dc237adead
ohunyanc <wildcardcharly@outlook.com>
2024-02-17 10:27:24 +0100

    commit

--- components/Features.jsx
+++ components/Features.jsx
@@ -19,8 +19,8 @@ const Features = () => {
           }
       }}
       viewport={{ once: true }}>Business Services</motion.h1>
-      <div className='w-auto flex h-[100%] md:flex-row sm:flex-col sm:flex-col-1 gap-8 p-12 items-center justify-between'>
-        <motion.div className="card card-compact w-76 basis-1/3 bg-textDark shadow-xl"
+      <div className='w-auto flex h-[100%] md:flex-row sm:flex-col gap-4 p-8 items-center justify-between'>
+        <motion.div className="card card-compact w-56 basis-1/3 bg-textDark shadow-xl"
           initial={{ opacity: 0, x: 10 }}
           whileInView={{
               opacity: 1,
@@ -33,14 +33,14 @@ const Features = () => {
           viewport={{ once: true }}>
           <figure><Image src={dribble1} alt="Shoes" /></figure>
           <div className="card-body">
-            <h2 className="card-title" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis repudiandae est dolor, asperiores consequuntur iusto!</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam dolore architecto, nihil natus libero vero!</p>
+            <h2 className="card-title sm:text-sm" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis repudiandae est dolor, asperiores consequuntur iusto!</h2>
+            <p className='sm:text-base'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam dolore architecto, nihil natus libero vero!</p>
             {/* <div className="card-actions justify-end">
               <button className="btn btn-primary">Buy Now</button>
             </div> */}
           </div>
         </motion.div>
-        <motion.div className="card card-compact w-76 basis-1/3 bg-textDark shadow-xl"
+        <motion.div className="card card-compact w-56 basis-1/3 bg-textDark shadow-xl"
           initial={{ opacity: 0, x: 10 }}
           whileInView={{
               opacity: 1,
@@ -53,14 +53,14 @@ const Features = () => {
           viewport={{ once: true }}>
           <figure><Image src={dribble2} alt="img" /></figure>
           <div className="card-body">
-            <h2 className="card-title">Explore...Tech and Society</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, eos. Eius dignissimos id veritatis facilis?</p>
+            <h2 className="card-title sm:text-sm">Explore...Tech and Society</h2>
+            <p className='sm:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, eos. Eius dignissimos id veritatis facilis?</p>
             {/* <div className="card-actions justify-end">
               <button className="btn btn-primary">Buy Now</button>
             </div> */}
           </div>
         </motion.div>
-        <motion.div className="card card-compact w-76 basis-1/3 bg-textDark shadow-xl"
+        <motion.div className="card card-compact w-56 basis-1/3 bg-textDark shadow-xl"
           initial={{ opacity: 0, x: 10 }}
           whileInView={{
               opacity: 1,
@@ -73,8 +73,8 @@ const Features = () => {
           viewport={{ once: true }}
           ><figure><Image src={dribble3} alt="img" /></figure>
           <div className="card-body">
-            <h2 className="card-title">Tech Exploration and Development</h2>
-            <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos. Praesentium cumque soluta nisi adipisci accusantium odio! Eius, eligendi maxime.</p>
+            <h2 className="card-title sm:text-sm">Tech Exploration and Development</h2>
+            <p className='sm:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, quos. Praesentium cumque soluta nisi adipisci accusantium odio! Eius, eligendi maxime.</p>
             {/* <div className="card-actions justify-end">
               <button className="btn btn-primary">Buy Now</button>
             </div> */}
